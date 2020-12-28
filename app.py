8e6eaee804116f274b3831c4011e6994f3267ccd
Robert Jensen <robert@robert-jensen.dk>
2020-12-28 20:49:43 +0000

    working googd enough to get data

---
+++ app.py
@@ -0,0 +1,5 @@
+# Get data from john hopkins
+# https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_daily_reports/12-27-2020.csv
+
+import pandas as pd
+
