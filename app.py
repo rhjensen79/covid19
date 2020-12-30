9799de4e110806da548f7eb0b226b7c70f31367c
Robert Jensen <robert@robert-jensen.dk>
2020-12-30 21:05:20 +0000

    working

--- app.py
+++ app.py
@@ -4,6 +4,7 @@
 import pandas as pd
 import requests 
 from bs4 import BeautifulSoup 
+import re
 
 
 #Global variables
@@ -17,19 +18,22 @@ for link in soup.find_all('a'):
     if link['href'].endswith('.csv'):
         csv_url = ("https://raw.githubusercontent.com"+link.get('href'))    #Create proper Raw link
         csv_url = csv_url.replace('/blob', '')                              #Create proper Raw link
-        date = csv_url[2:-4]
+
+        find_date = re.search('\d{2}-\d{2}-\d{4}', csv_url)                 #Find date in url
+        date = find_date.group(0)                                           #match date in url
+
         print (csv_url)                                                     #Print converted URL
-        data = pd.read_csv(csv_url, index_col=None, header=0)
-        #data = pd.read_csv(csv_url) 
-        if 'Denmark' in data.values:
+        data = pd.read_csv(csv_url, index_col=None, header=0)               #Get data from url
+
+        data.insert(0, 'Date', date)                                        #Add date to Dataframe
+
+        if 'Denmark' in data.values:                                        #Search for Denmark in data.
             if 'Country/Region' in data:                                    #Cleanup colum names
                 data = data.rename(columns = {'Country/Region': 'Country_Region'}, inplace = False)
-
-            
+     
             data = data[(data['Country_Region'] == 'Denmark') ]             #Add Denmark data
             denmark.append(data)                                            #Add Append data to existing dataframe
 
-        
 #Cleanup Dataframe
 denmark = pd.concat(denmark, axis=0, ignore_index=True)        
 denmark = denmark[(denmark['Country_Region'] == 'Denmark') & (denmark['Province_State'] != 'Faroe Islands') & (denmark['Province_State'] != 'Greenland')]
